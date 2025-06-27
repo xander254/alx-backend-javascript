7fcfa94255f7a34f1ed3c7695f90850641d94089
xander254 <alexkinyanjui356@gmail.com>
2025-06-27 05:27:48 +0300

    publisher

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,23 @@
+import redis from "redis";
+
+const client = redis.createClient();
+
+client.on("connect", () => {
+  console.log("Redis client connected to the server");
+});
+
+client.on("error", (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish("ALXchannel", message);
+  }, time);
+}
+
+publishMessage("ALX Student #1 starts course", 100);
+publishMessage("ALX Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("ALX Student #3 starts course", 400);
