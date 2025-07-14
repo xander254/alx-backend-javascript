260f2be71a9f84f1a2e28c0f2ba0e5562d03a029
xander254 <alexkinyanjui356@gmail.com>
2025-07-15 01:34:16 +0300

    Using Process stdin: refactor

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
 process.stdout.write("Welcome to ALX, what is your name?\n");
-
+process.stdin.setEncoding("utf8");
 process.stdin.on("data", (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+  console.log("This important software is now closing");
 });
